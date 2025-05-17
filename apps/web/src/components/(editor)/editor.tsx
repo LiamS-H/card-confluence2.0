4e559d48e7c0a8dedd0c8d985db846d994b55c1e
liamsh <liamsh@gmail.com>
2025-05-17 23:47:54 +0100

    add connecting scrycards language and card-confluence

--- apps/web/src/components/(editor)/editor.tsx
+++ apps/web/src/components/(editor)/editor.tsx
@@ -3,6 +3,7 @@ import ReactCodeEditor from "@uiw/react-codemirror";
 import { keymap } from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
 import { useState } from "react";
+import { scrycards } from "codemirror-lang-scrycards";
 
 const INITIAL = `game:arena is:permanent (o:draw or o:reveal)
 o:enters (order:cmc (direction:arena (o:"test o" or o:/[\\/]+/)))
@@ -10,7 +11,7 @@ o:enters (order:cmc (direction:arena (o:"test o" or o:/[\\/]+/)))
 
 export function ScrycardsEditor() {
     const [doc, setDoc] = useState(INITIAL);
-    const extensions = [keymap.of(defaultKeymap)];
+    const extensions = [keymap.of(defaultKeymap), scrycards()];
     return (
         <ReactCodeEditor
             extensions={extensions}
