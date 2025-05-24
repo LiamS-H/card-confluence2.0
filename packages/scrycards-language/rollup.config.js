3ae2e8966e433f5b7899d8a7d6fd9fb5bdf27ddd
LiamS-H <liamsh@gmail.com>
2025-05-24 14:06:17 -0700

    fix rollup config

--- packages/scrycards-language/rollup.config.js
+++ packages/scrycards-language/rollup.config.js
@@ -5,10 +5,10 @@ import { lezer } from "@lezer/generator/rollup";
 export default {
     input: "src/index.ts",
     external: (id) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
-    // output: [
-    //     { file: "dist/index.cjs", format: "cjs" },
-    //     { dir: "./dist", format: "es" },
-    //     // { file: "dist/index.js", format: "es" },
-    // ],
+    output: [
+        // { file: "dist/index.cjs", format: "cjs" },
+        // { dir: "./dist", format: "es" },
+        { file: "dist/index.js", format: "es" },
+    ],
     plugins: [lezer(), typescript()],
 };
